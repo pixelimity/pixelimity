ce620bcb5ac28c27d6341ecfb8f62a97e569a6d5
aufa <neonwebmedia@yahoo.com>
2014-08-14 13:21:14 +0700

    fix protocol & URL https support
    Added helper

--- install/index.php
+++ install/index.php
@@ -3,11 +3,53 @@
  * Installer file to make the site ready to used
  * @package pixelimity
  */
-define ('BASE', str_replace("\\", "/", dirname(dirname(__FILE__))), true);
-define ('PATH', str_replace(rtrim($_SERVER['DOCUMENT_ROOT'], '/'), '', BASE));
-define ('DIR', BASE);
-define ('URL', 'http://'. $_SERVER['HTTP_HOST'] . PATH);
+/* ------------------------------------------------------------------------------------------------------ \
+                                        HELPER
+\ ------------------------------------------------------------------------------------------------------ */
 
+/**
+ * isset( $_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == '443' # port 443 is not always for ssl
+ * load balancer fix that has x forwarder protocol set with. add $_SERVER['HTTPS'] -> 'on' value
+ */
+if (isset( $_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+    $_SERVER['HTTPS'] = 'on';
+}
+
+/* GETTING PROTOCOL 
+----------------------------------------- */
+$protocol = ( isset($_SERVER['REQUEST_SCHEME']) && $_SERVER['REQUEST_SCHEME'] == 'https' ) ||
+            ( isset( $_SERVER['HTTPS']) && ( strtolower( $_SERVER['HTTPS']) == 'on' || $_SERVER['HTTPS'] == '1' ) )
+            ? 'https' : 'http';
+
+/* GETTING DOCUMENT ROOT USING REALPATH AVOID SYMLINK
+----------------------------------------- */
+$docroot = function_exists('realpath') ? realpath($_SERVER['DOCUMENT_ROOT']) : $_SERVER['DOCUMENT_ROOT'];
+// clean path ( windows directory separator fix )
+$docroot = rtrim(str_replace( array( "\\", "\\\\" ), "/", $docroot ), '/');
+
+/* GETTING DOCUMENT ROOT USING REALPATH AVOID DIR SYMLINK IF PROVIDE
+----------------------------------------- */
+$base    = function_exists('realpath') ? realpath(dirname(__FILE__)) : dirname(__FILE__);
+$base    = str_replace( array( "\\", "\\\\" ), "/", $base );
+
+/* GETTING HOST AS DOMAIN
+----------------------------------------- */
+$host    = ( isset( $_SERVER['HTTP_HOST'] ) ? $_SERVER['HTTP_HOST'] : isset( $_SERVER['SERVER_NAME'] ) ? $_SERVER['SERVER_NAME'] : false );
+// if no host exits
+if( ! $host ) :
+    header( 'HTTP/1.1 400 Bad Request');
+    die('Server Not Supported, HTTP_HOST / SERVER_NAME is not defined!');
+endif;
+
+/* ------------------------------------------------------------------------------------------------------ \
+                                        END HELPER
+\ ------------------------------------------------------------------------------------------------------ */
+
+/* Define some parameters */
+define ('BASE', $base);
+define ('PATH', '/'.trim(str_replace( $docroot, '', BASE ), '/') );
+define ('DIR',  $base );
+define ('URL',  rtrim( $protocol.'://'. $host.PATH, '/' ) );
 if (file_exists(DIR .'/install/config.php')) :
     header("Location: ". URL);
 endif;
