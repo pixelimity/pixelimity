d405592db12c6dd1355e145f7e41f64bd5cfbb41
Septyaman Vom <septyvombervom@gmail.com>
2014-12-26 23:13:36 +0700

    Create session.php
    menambah sebuah class

---
+++ core/class/session.php
@@ -0,0 +1,44 @@
+<?php if (!defined('PXL')) handle_error('PXL_Reqruitments',' Please Define IT');
+
+/*
+   Pixelimity CMS (https://github.com/pixelimity/pixelimity/)
+   About : Sebuah clas untuk menangani sesi pada aplikasi
+   Status : Maintenance
+*/
+
+class pixelimity_session{
+
+  // [!] Masih kerangka
+  
+  private user=null;
+  private login=false;
+  
+  
+  function __construct(){
+    
+     $this->check_login();
+  
+  }
+  
+  private function check_login(){
+     // example
+    $this-> login = false;
+  
+  }
+  
+  
+  /*
+    
+    Masih Kerangka !
+  
+  */
+
+
+
+
+
+
+
+
+
+}
